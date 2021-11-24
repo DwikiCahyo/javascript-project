b6ae397ea12eea3f0d2f1fbf2a12edc3d6e52f7d
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-11-24 16:20:24 +0700

    commit

--- server.js
+++ server.js
@@ -4,7 +4,41 @@ const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
  
     res.statusCode = 200;
-    res.end('<h1>Halo HTTP Server!</h1>');
+    // res.end('<h1>Halo HTTP Server!</h1>');
+
+
+
+//Method/Verb Request
+const method =req.method;
+
+//memamnggil nilai dalam method("get","POST","PUT")
+if(method=='GET'){
+    res.end('<h1>Hello!</h1>');
+}
+//Membuat Body request pada method post
+if(method === 'POST') {
+    let body  =[];
+
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    req.on('end', () => {
+        body = Buffer.concat(body).toString();
+        const {name}=JSON.parse(body);
+        res.end(`<h1>Hai, ${name}!</h1>`);
+    });
+
+    // res.end('<h1>Hai!</h2>');
+}
+
+if(method === 'PUT') {
+    res.end('<h1>Bonjour!</h1>');
+}
+if(method === 'DELETE') {
+    res.end('<h1>Salam!</h1>');
+}
+
 };
 //Deklarasi variabel server
 const server = http.createServer(requestListener);
